76238f76795e1b87cc48a8e95e02821303198b94
BeABraveDude <beabravedude@gmail.com>
2024-06-04 16:13:02 -0400

    Started email stuff.

--- actions/mail.ts
+++ actions/mail.ts
@@ -1,3 +1,13 @@
 'use server';
 
 // TODO Add email logic with some library
+import {mailTransport} from "@/lib/email";
+
+export const sendTestEmail = async (email: string) => {
+    return mailTransport.sendMail({
+        from: "beabravedude@gmail.com",
+        to: email,
+        subject: "Testing my Nodemailer/SES setup",
+        text: "This is a message to say you did it!",
+    });
+}
--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,16 +1,6 @@
-const VATUSA_API = 'https://api.vatusa.net/v2';
-const VATUSA_KEY = process.env.VATUSA_API_KEY;
+import {sendTestEmail} from "@/actions/mail";
 
 export async function GET() {
-
-    const res = await fetch(`${VATUSA_API}/user/1621075/training/records?apiKey=${VATUSA_KEY}`, {
-        headers: {
-            'Authorization': `Bearer ${VATUSA_KEY}`,
-            'Content-Type': 'application/json',
-        },
-    });
-
-    console.log(res.url);
-
-    return Response.json(await res.json());
+    const res = await sendTestEmail('');
+    return Response.json(res);
 }
