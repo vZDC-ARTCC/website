7abfa8438331ef89ba11a7b8518db382991ffad5
Harry Xu <harryxu2626@gmail.com>
2024-06-20 18:05:00 -0400

    automatically strip leading K from airport codes

--- actions/prd.ts
+++ actions/prd.ts
@@ -4,6 +4,8 @@ import {PreferredRoute} from "@/types";
 
 export const getPrdRoutes = async (origin?: string, destination?: string) => {
     if (!origin || !destination) return [];
+    if(origin.toLowerCase().startsWith('k')) origin = origin.substring(1);
+    if(destination.toLowerCase().startsWith('k')) destination = destination.substring(1);
     const res = await fetch(`https://api.aviationapi.com/v1/preferred-routes/search?origin=${origin}&dest=${destination}`);
     if (!res.ok) return [];
     const data: PreferredRoute[] = await res.json();
