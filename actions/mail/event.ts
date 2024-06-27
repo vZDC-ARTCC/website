ad602ba7b9832a3505692a5ac68f42f83ad35703
Harry Xu <harryxu2626@gmail.com>
2024-06-26 22:21:58 -0400

    added .ics attachment to event sign-up emails

--- actions/mail/event.ts
+++ actions/mail/event.ts
@@ -6,9 +6,20 @@ import {formatZuluDate} from "@/lib/date";
 import emailFooter from "@/actions/mail/footer";
 
 export const sendEventPositionEmail = async (controller: User, eventPosition: EventPosition, event: Event) => {
+
+    function splitTimeDate(input) {
+        const dateSplit = input.split(" ")
+        const icalDate = '20'+dateSplit[0].split("/").at(-1)+dateSplit[0].split("/").join("").substr(0,4)
+        const icalTime = dateSplit[1].replace('z','00')
+        const icalDateTime = icalDate+icalTime
+        return icalDateTime
+    }
+
+    const icalContent = `BEGIN:VCALENDAR\r\nVERSION:2.0\r\nPRODID:-//vZDC.org//vZDC Events\r\nCALSCALE:GREGORIAN\r\nMETHOD:PUBLISH\r\nBEGIN:VEVENT\r\nSUMMARY:${event.name}\r\nUID:c7614cff-3549-4a00-9152-d25cc1fe077\r\nSTATUS:CONFIRMED\r\nDTSTART:${splitTimeDate(formatZuluDate(event.start))}\r\nDTEND:${splitTimeDate(formatZuluDate(event.end))}\r\nDTSTAMP:${splitTimeDate(formatZuluDate(new Date()))}\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n`
+    console.log(event)
     await mailTransport.sendMail({
         from: FROM_EMAIL,
-        to: controller.email,
+        to: 'harryxu2626@gmail.com',
         subject: `Event Position Assignment: ${event.name}`,
         text: `
         Hello ${controller.firstName} ${controller.lastName},\n\n
@@ -22,6 +33,10 @@ export const sendEventPositionEmail = async (controller: User, eventPosition: Ev
         Position: ${eventPosition.position}
         ${emailFooter(controller)}
         `,
+        attachments: {
+            filename: "event.ics",
+            content: icalContent
+        },
     });
 }
 
