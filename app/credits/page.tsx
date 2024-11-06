1fbc64423924c3b769023b9a297e57c364c65ae3
Carson Berget <142100426+Vainnor@users.noreply.github.com>
2024-11-06 17:39:38 -0500

    Updated credits page

--- app/credits/page.tsx
+++ app/credits/page.tsx
@@ -1,12 +1,11 @@
 import {Card, CardContent, Grid, Typography} from "@mui/material";
-import {BugReportSharp, Info, SettingsSuggest} from '@mui/icons-material'
 import Link from "next/link";
 
 export default async function Home() {
 
     return (
-        <Grid container columns={9} spacing={4}>
-            <Grid item xs={9}>
+        <Grid container columns={6} spacing={4}>
+            <Grid item xs={6}>
                 <Card>
                     <CardContent>
                         <Typography variant="h4">Credit Information</Typography>
@@ -16,7 +15,7 @@ export default async function Home() {
                     </CardContent>
                 </Card>
             </Grid>
-            <Grid item xs={3}>
+            <Grid item xs={6}>
                 <Card>
                     <CardContent>
                         <Typography variant="h5" sx={{mb: 1,}}>Main Contributors</Typography>
@@ -24,35 +23,6 @@ export default async function Home() {
                         <Typography variant="h6" color="#29B6F6" fontWeight="bold">Carson Berget</Typography>
                         <Typography variant="h6" color="#29B6F6" fontWeight="bold">Leo Roberts</Typography>
                         <Typography variant="h6" color="#29B6F6" fontWeight="bold">Harry Xu</Typography>
-                    </CardContent>
-                </Card>
-            </Grid>
-            <Grid item xs={3}>
-                <Card>
-                    <CardContent>
-                        <Typography variant="h5" sx={{mb: 1,}}>Bug/Suggestion Report</Typography>
-                        <Typography variant="body1" color="#F44336"
-                                    sx={{display: 'flex', gap: 1, alignItems: 'center',}}><SettingsSuggest/> Cameron D.</Typography>
-                        <Typography variant="body1" color="#F44336"
-                                    sx={{display: 'flex', gap: 1, alignItems: 'center',}}><BugReportSharp/> James Martin</Typography>
-                        <Typography variant="body1" color="#F44336"
-                                    sx={{display: 'flex', gap: 1, alignItems: 'center',}}><SettingsSuggest/> Junzhe Yan</Typography>
-                        <Typography variant="body1" color="#F44336"
-                                    sx={{display: 'flex', gap: 1, alignItems: 'center',}}><SettingsSuggest/> Geoff Burns</Typography>
-                        <Typography variant="body1" color="#F44336"
-                                    sx={{display: 'flex', gap: 1, alignItems: 'center',}}><BugReportSharp/> John
-                            Man</Typography>
-
-                    </CardContent>
-                </Card>
-            </Grid>
-            <Grid item xs={3}>
-                <Card>
-                    <CardContent>
-                        <Typography variant="h5" sx={{mb: 1,}}>Easter Egg</Typography>
-                        <Typography variant="body1" color="#CE93D8"
-                                    sx={{display: 'flex', gap: 1, alignItems: 'center',}}><Info/>Coming
-                            Soon</Typography>
                     </CardContent>
                 </Card>
             </Grid>
